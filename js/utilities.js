4514e0c00a4dfa4dddf8d31f93353d538d578bac
Tarik Uyan <musician.tarik@gmx.de>
2023-05-14 14:16:39 +0200

    favicon added, debounce added to utilities

--- js/utilities.js
+++ js/utilities.js
@@ -1,19 +1,36 @@
 const log = console.log;
 
-const $ = selector => document.querySelector(selector);
-const $$ = selector => document.querySelectorAll(selector);
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
-const show = selector => $(selector).classList.remove('d-none'); 
-const hide = selector => $(selector).classList.add('d-none'); 
+const show = (selector) => $(selector).classList.remove("d-none");
+const hide = (selector) => $(selector).classList.add("d-none");
 
 const toggleActiveBtn = (buttons) => {
-    buttons.forEach(button => button.classList.toggle('active', button == event.currentTarget))
-}
+  buttons.forEach((button) =>
+    button.classList.toggle("active", button == event.currentTarget)
+  );
+};
 
 const addNavToggleBtns = () => {
-    $$('nav').forEach(
-        navbar => navbar.querySelectorAll('button').forEach(
-            button => button.addEventListener('click', () => toggleActiveBtn(navbar.querySelectorAll('button')))
+  $$("nav").forEach((navbar) =>
+    navbar
+      .querySelectorAll("button")
+      .forEach((button) =>
+        button.addEventListener("click", () =>
+          toggleActiveBtn(navbar.querySelectorAll("button"))
         )
-    )
-}
+      )
+  );
+};
+
+const debounce = (cb, delay = 1000) => {
+  let timeout;
+
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+};
