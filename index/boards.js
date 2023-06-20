fbc4e2597b47c635a87c0c8e0adfac4cc9d09634
Lukas Busch <lukas.busch1996@gmx.at>
2023-06-20 18:50:26 +0200

    i tried...

--- index/boards.js
+++ index/boards.js
@@ -5,28 +5,26 @@ const addTaskModal = async () => {
 
 const addDragAndDrop = () => {
     const task = event.currentTarget;
-    const currentPosition = {
-        mouseX: event.pageX,
-        mouseY: event.pageY,
-        currentX: Number(event.currentTarget.style.getPropertyValue('--x')),
-        currentY: Number(event.currentTarget.style.getPropertyValue('--y'))
+    const startingPosition = {
+        startingX: event.pageX,
+        startingY: event.pageY,
     }
     task.addEventListener("mousemove", dragHandler = () => {
-        taskDragger(currentPosition);
+        taskDragger(startingPosition);
     });
     // task.addEventListener("mouseleave", () => {
     //     taskDropper(event, task);
     //     task.removeEventListener("mousemove", dragHandler);
     // }, { once: true });
-    document.addEventListener("mouseup", () => {
-        taskDropper(event, task);
+    document.addEventListener("mouseup", (event) => {
+        taskDropper(event, task, startingPosition);
         task.removeEventListener("mousemove", dragHandler);
     }, { once: true });
 }
 
-const taskDragger = throttle(({ mouseX, mouseY, currentX, currentY  }) => {
-    event.currentTarget.style.setProperty('--x', currentX + event.pageX - mouseX);
-    event.currentTarget.style.setProperty('--y', currentY + event.pageY - mouseY);
+const taskDragger = throttle(({ startingX, startingY }) => {
+    event.currentTarget.style.setProperty('--x', event.pageX - startingX);
+    event.currentTarget.style.setProperty('--y', event.pageY - startingY);
     checkPlaceholder(event);
 }, 10)
 
@@ -44,20 +42,23 @@ const checkPlaceholder = ({pageX, pageY}) => {
     })
 }
 
-const taskDropper = ({pageX, pageY}, task) => {
+const taskDropper = ({pageX, pageY, offsetX, offsetY}, task, { startingX, startingY }) => {
     $$('#tasks > div').for(taskWrapper => {
-        const { x, y, width, height } = taskWrapper.getBoundingClientRect();
-        if (x < pageX && pageX < (x + width) &&
-            y < pageY && pageY < (y + height)) {
-                taskWrapper.classList.remove('placeholder');
-                taskWrapper.append(task)
+        const { x: x1, y: y1, width, height } = taskWrapper.getBoundingClientRect();
+        if (x1 < pageX && pageX < (x1 + width) &&
+            y1 < pageY && pageY < (y1 + height)) {
+            taskWrapper.classList.remove('placeholder');
+            taskWrapper.append(task)
+            const { x, y, width, height } = task.getBoundingClientRect();
+            // task.style.setProperty('--x', `${pageX - x1 - offsetX}`);
+            task.style.setProperty('--x', `${0}`);
+            task.style.setProperty('--y', `${y - y1}`);
         }
     })
-    task.classList.add('drop-transition');
-    task.addEventListener("transitionend", () => {
-        task.classList.remove('drop-transition');
-        log("transition removed!")
-    }, { once: true });
-    task.style.setProperty('--x', '0');
-    task.style.setProperty('--y', '0');
+    // task.classList.add('drop-transition');
+    // task.addEventListener("transitionend", () => {
+    //     task.classList.remove('drop-transition');
+    // }, { once: true });
+    // task.style.setProperty('--x', '0');
+    // task.style.setProperty('--y', '0');
 }
