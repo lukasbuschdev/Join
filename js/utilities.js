ab042f8d5a86066d21824c8a313374f5deed847c
Tarik Uyan <musician.tarik@gmx.de>
2023-06-20 08:24:50 +0200

    board dragger added

--- js/utilities.js
+++ js/utilities.js
@@ -36,6 +36,21 @@ const debounce = (cb, delay = 1000) => {
   };
 };
 
+function throttle (cb, delay = 1000) {
+  let shouldWait = false;
+
+  return (...args) => {
+      if (shouldWait) return;
+
+      cb(...args);
+      shouldWait = true;
+
+      setTimeout(() => {
+          shouldWait = false;
+      }, delay);
+  }
+}
+
 const includeTemplates = async () => {
   $$('[include-template]').forEach(
     async (templateContainer) => await includeTemplate(templateContainer)
