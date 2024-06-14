23fb35df2ed2211956af720f04810acc16f931fe
Lukas Busch <lukas.busch1996@gmx.at>
2024-06-14 17:49:58 -0600

    lang bug fixed

--- init/signup/signup.js
+++ init/signup/signup.js
@@ -7,11 +7,10 @@ const initSignup = () => {
 }
 
 const initPrivacyLink = () => {
-    log('ISch werd ausjeführt!')
     const privacyContainer = $('[data-lang="register-privacy"]');  
-    privacyContainer.innerHTML = privacyContainer.innerHTML.replace(/%.+%/, (words) => {
+    privacyContainer.innerHTML = privacyContainer.innerHTML.replace(/%(.+)%/, (match, words) => {
         return /*html*/ `
-            <span class="txt-blue" style="cursor: pointer;" onclick="initPrivacy()">${words.replaceAll('%', '')}</span>
+            <span class="txt-blue" style="cursor: pointer;" onclick="initPrivacy()">${words}</span>
         `});
 }
 
@@ -28,6 +27,7 @@ const validateInputs = async ({ name, email, password, confirmPassword }) => {
     const passwordValidity = invalidPassword(password);
     const differentPasswords = password !== confirmPassword;
     const privacyAccepted = !($('[type="checkbox"]').checked);
+    const legalNoticeAccepted = !($('[type="checkbox"]').checked);
 
     throwErrors(
         { identifier: 'invalid-name', bool: nameValidity },
@@ -36,16 +36,18 @@ const validateInputs = async ({ name, email, password, confirmPassword }) => {
         { identifier: 'email-in-use', bool: emailInUse },    
         { identifier: 'invalid-password', bool: passwordValidity },
         { identifier: 'different-passwords', bool: differentPasswords },
-        { identifier: 'accept-privacy', bool: privacyAccepted }
+        { identifier: 'accept-privacy', bool: privacyAccepted },
+        { identifier: 'accept-legal-notice', bool: legalNoticeAccepted }
     );
 
-    if (nameValidity == false &&
+    if(nameValidity == false &&
         nameInUse == false &&
         emailValidity == false &&
         passwordValidity == false &&
         emailInUse == false &&
         differentPasswords == false &&
-        privacyAccepted == false) {
+        privacyAccepted == false &&
+        legalNoticeAccepted == false) {
         return true;
     }
     return false;
@@ -61,11 +63,10 @@ const signupInit = async () => {
         confirmPassword: $('#confirm-password input').value
     }
 
-    if (await validateInputs(dataInput) == false) return;
+    if(await validateInputs(dataInput) == false) return;
 
     const hash = await hashInputValue(dataInput.password);
     dataInput.password = hash;
     const user = new User(dataInput);
     user.initVerification();
 }
-
