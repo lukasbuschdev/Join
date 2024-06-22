15a21f74b09015b25c291696e25ac90e19efc0b6
Tarik Uyan <musician.tarik@gmx.de>
2024-06-23 00:31:40 +0200

    hukuki bildirim

--- index/legal_notice/legal_notice.js
+++ index/legal_notice/legal_notice.js
@@ -1,7 +1,13 @@
-import { bindInlineFunctions, getContext } from "../../js/setup.js";
-
+import { bindInlineFunctions, getContext } from "/Join/js/setup.js";
 bindInlineFunctions(getContext(), [
     '/Join/index/index/index.js',
     '/Join/js/language.js',
     '/Join/js/utilities.js'
-])
+]);
+
+import { $ } from "../../js/utilities.js";
+
+
+export function initLegalNotice() {
+    ($('#content') ?? $('body')).includeTemplate({url: `/Join/assets/templates/init/legal-notice-content.html`, replace: false});
+}
