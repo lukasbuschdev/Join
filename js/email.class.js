5038eec9fe4de8744d6e77307f6c8599bc051018
Tarik Uyan <musician.tarik@gmx.de>
2024-07-20 01:45:53 +0200

    next hotfix

--- js/email.class.js
+++ js/email.class.js
@@ -2,29 +2,25 @@ import { STORAGE } from "./storage.js";
 import { resetPasswordEmailTemplate, verificationEmailTemplate } from "/Join/assets/templates/index/mail_templates.js";
 
 export class Email {
-  constructor({ recipient, type = "", langData }) {
-    this.recipient = recipient;
-    this.subject = langData["subject"];
-    if (type == "verification") {
-      this.message = verificationEmailTemplate(recipient, langData);
-    }
-    if (type == "passwordReset") {
-      this.message = resetPasswordEmailTemplate(recipient, langData);
-    }
-  }
+	constructor({ recipient, type = "", langData }) {
+		this.recipient = recipient;
+		this.subject = langData["subject"];
+		if (type == "verification") this.message = verificationEmailTemplate(recipient, langData);
+		if (type == "passwordReset") this.message = resetPasswordEmailTemplate(recipient, langData);
+	}
 
-  async send() {
-    const { socket } = STORAGE.webSocket;
-    socket.init(this.recipient);
-    const mailOptions = {
-      to: this.recipient.email,
-      subject: this.subject,
-      html: this.message
-    };
-    socket.emit("mail", mailOptions);
-    return new Promise((resolve, reject) => {
-      socket.on("mailSent", resolve);
-      socket.on("mailFailed", reject);
-    });
-  }
+	async send() {
+		const { socket } = STORAGE.webSocket;
+		STORAGE.webSocket.init(this.recipient);
+		const mailOptions = {
+			to: this.recipient.email,
+			subject: this.subject,
+			html: this.message
+		};
+		socket.emit("mail", mailOptions);
+		return new Promise((resolve, reject) => {
+			socket.on("mailSent", resolve);
+			socket.on("mailFailed", reject);
+		});
+	}
 }
