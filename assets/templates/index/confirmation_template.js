83b212a08b1f9a2f554ee6102d7a212eccb15484
lukasbuschdev <buschlukas175@gmail.com>
2024-07-19 20:33:12 -0600

    added delete-account-button  & confirmations

--- assets/templates/index/confirmation_template.js
+++ assets/templates/index/confirmation_template.js
@@ -2,7 +2,16 @@ import "/Join/js/prototype_extensions.js";
 
 /**
  * gets the template for the confirmation box depending on the type
- * @param {"delete-contact" | "delete-category" | "delete-board", | "delete-task" | "reset-password" | "save-edited-task" | "save-edited-board" | "logout"} type
+ * @param {"delete-contact" | "delete-category" | "delete-board", | "delete-task" | "confirmation-delete-account1": "Do you really want to delete your account?",
+	"confirm-delete-account1": "I do", 
+	"confirmation-delete-account2": "Are you sure?",
+	"confirm-delete-account2": "I am",
+	"confirmation-delete-account3": "Really?",
+	"confirm-delete-account3": "REALLY!",  
+	"confirmation-delete-account4": "This account cannot be deleted!",
+
+	"reset-password" | "save-edited-task" | "save-edited-board" | "logout"} type
+	"delete-account": "Delete Account",
  * @returns {string}
  */
 export function confirmationTemplate(type) {
